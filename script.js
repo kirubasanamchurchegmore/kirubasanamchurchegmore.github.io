6ee2a52340ee9e8a5c28d679dab8b73a409cb92a
dhivyanj <70884995+kirubasanamchurchegmore@users.noreply.github.com>
2021-06-09 22:20:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ function detectMob() {
     return ( ( window.innerWidth <= 800 ) && ( window.innerHeight <= 600 ) );
   }
 
-if (check == true) {
+if (detectMob() == true) {
     window.location.href = "https://kirubasanamchurchegmore.github.io/m/";
 }
